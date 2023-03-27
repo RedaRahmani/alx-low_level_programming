fa822ecf84115eb452dde9612959ea876d3e7b76
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2023-03-27 15:42:35 -0700

    C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * rev_string 
- * */
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
+ */
 
 void rev_string(char *s)
 {
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- * print_array 
- * */
+ * print_array - a function that prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements OF the array to be printed
+ * Return: a and n inputs
+ */
 void print_array(int *a, int n)
 {
 	int i;
@@ -17,5 +20,4 @@ void print_array(int *a, int n)
 		}
 			printf("\n");
 }
-
 
