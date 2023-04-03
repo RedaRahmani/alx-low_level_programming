99c646e716324b03200262bb5edcedec97e4fbec
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2023-04-03 16:30:11 -0700

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset -
+ *
+ *  * Return: changed array with a new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
+
