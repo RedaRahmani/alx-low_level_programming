63e5d0092b1293b301bcbb2678238abfc2234bf6
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2023-05-02 13:13:34 -0700

    test

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,7 +8,7 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t Temp = 0;
 
 	if (h == NULL)
 	{
@@ -19,10 +19,10 @@ size_t print_listint(const listint_t *h)
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		num++;
+		Temp++;
 		h = h->next;
 	}
 
-	return (num);
+	return (Temp);
 }
 
