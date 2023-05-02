9796e69631d9404d2a726ebefb0753ee53b10909
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2023-05-02 13:05:01 -0700

    test

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	if (h == NULL)
+	{
+		printf("La liste est vide\n");
+		return (0);
+	}
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
+
