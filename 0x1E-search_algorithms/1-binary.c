2c719477a46e97faa1b4442148da429c1f695f15
RedaRahmani <mohamedredarahmani20@ump.ac.ma>
2023-11-05 06:47:43 -0800

    task01

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,81 +1,40 @@
 #include "search_algos.h"
+
 /**
- * binary_search - returns first index of where the value is located
+ * binary_search - searches for a value in a sorted array
+ *	binary search algorithm using integers
  *
- * @array: pointer to array
- * @size: size of array
+ * @array: pointer to the first element of the array to the search
+ * @size: number of elements in the array
  * @value: value to search for
  *
- * Return: index of the value, otherwise -1
+ * Return: index where the value is located,
+ *	or -1 if the value is not present or if the array is NULL.
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t left = 0, i, mid;
+	size_t right = size - 1;
+
 	if (array == NULL)
 		return (-1);
-	else
-		return (binary_search_rec(array, 0, size - 1, value));
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[right]);
+
+		mid = (left + right) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
 
 	return (-1);
-}
-
-
-/**
- * binary_search_rec - returns first index of where the value is located
- *
- * @array: pointer to array
- * @low: start index
- * @high: last index
- * @value: value to search for
- *
- * Return: index of the value, otherwise -1
- */
-int binary_search_rec(int *array, int low, int high, int value)
-{
-	unsigned int mid;
-
-	mid = ((high - low) / 2) + low;
-
-	if (low > high)
-		return (-1);
-
-	print_array(array, low, high + 1);
-
-	if (array[mid] == value)
-		return (array[mid]);
-
-	if (array[mid] > value)
-	{
-		high = mid - 1;
-		return (binary_search_rec(array, 0, high, value));
-	}
-	else
-	{
-		low = mid + 1;
-		return (binary_search_rec(array, low, high, value));
-	}
-}
-
-
-/**
- * print_array - prints the array and subarray
- *
- * @array: the array
- * @size: size of the array
- * @low: index of the first element
- *
- * Return: Void
- */
-void print_array(int *array, int low, size_t size)
-{
-	size_t i = low;
-
-	printf("Searching in array:");
-	for (; i < size; i++)
-	{
-		printf(" %d", array[i]);
-		if (i < size - 1)
-			printf(",");
-		else
-			printf("\n");
-	}
 }
